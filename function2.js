7476525bb2e938b00116585c13cc6f0218073737
MalikAbdullahKorkmaz <161486498+MalikAbdullahKorkmaz@users.noreply.github.com>
2024-09-26 13:20:46 +0700

    Update function2.js

--- function2.js
+++ function2.js
@@ -60,6 +60,49 @@ let getTitle = () =>{
 
 console.log(fullname("John","Dream","Myung"));
 
+// Using the == operator (loose equality)
+console.log(5 == "5");  // true, because "5" is converted to the number 5
+
+// Using the === operator (strict equality)
+console.log(5 === "5"); // false, because the types are different (number vs string)
+// Calculator constructor function
+function Calculator() {
+  this.num1 = 10;
+  this.num2 = 20;
+
+  // Regular function: 'this' context depends on how the function is called
+  this.addRegular = function() {
+    console.log("Regular Function 'this':", this); // 'this' refers to the Calculator instance
+    return this.num1 + this.num2; // 'this' points to the Calculator object
+  };
+
+  // Arrow function: 'this' is lexically bound to the surrounding context
+  this.addArrow = () => {
+    console.log("Arrow Function 'this':", this); // 'this' still refers to the Calculator instance
+    return this.num1 + this.num2; // 'this' points to the Calculator object
+  };
+
+  // Regular function: has access to 'arguments' object
+  this.multiplyRegular = function() {
+    console.log("Regular Function 'arguments':", arguments); // 'arguments' is available
+    return arguments[0] * arguments[1];
+  };
+
+  // Arrow function: does not have access to 'arguments' object
+  this.multiplyArrow = (...args) => {
+    console.log("Arrow Function 'arguments':", args); // Uses rest parameters instead of 'arguments'
+    return args[0] * args[1];
+  };
+}
+
+// Create a new Calculator object
+const calc = new Calculator();
+
+console.log("Regular Function Add:", calc.addRegular());  // 30
+console.log("Arrow Function Add:", calc.addArrow());      // 30
+
+console.log("Regular Function Multiply:", calc.multiplyRegular(3, 4));  // 12
+console.log("Arrow Function Multiply:", calc.multiplyArrow(3, 4));      // 12
 
 
 
